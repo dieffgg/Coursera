92485d427f61076322f5c0106f20ce76bb95e67c
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2019-07-23 13:36:08 -0300

    colocando um texto na intro

--- adjacente.py
+++ adjacente.py
@@ -1,4 +1,4 @@
-# Escreva um programa que receba um número inteiro na entrada e verifique se o número recebido possui ao menos um dígito com um dígito adjacente igual a ele. Caso exista, imprima "sim"; se não existir, imprima "não"
+# Escreva um programa que receba um número inteiro na entrada e verifique se o número recebido possui ao menos um dígito com um dígito adjacente igual a ele. Caso exista, imprima "sim"; se não existir, imprima "não" comentario git
 
 ndepois = n = int(input("Digite um número inteiro: "))
 anterior = n % 10 
